80044815217e9c6fb955c4413bcc6a5a221006c8
Marie Gillier <magillier@hotmail.fr>
2018-06-12 14:17:43 +0200

    created userBar

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ function Rocket (){
 }
 
 Rocket.prototype.move = function (){
-  xRocket --;
+  xRocket--;
 }
 
 Rocket.prototype.hit = function (){
@@ -72,16 +72,61 @@ Rocket.prototype.hit = function (){
 //}
 
 
-// var user = {
-//   x:
-//   y:
+function UserBar () {
+  this.x = 390;
+  this.y = 380;
+  document.onkeydown = (e) => {
+    if (e.keyCode === 37) {
+      this.move(-1);
+    } else if (e.keyCode === 37) {
+      this.move(-1);
+    }
+  }
+}
 
+//UserBar.prototype.draw = function (){
+  //var img = new Image();
+  //this.img.src =
+//   ctx.beginPath();
+//   ctx.drawImage(this.img, this.x, this.y, 20, 20);
+//   ctx.closePath();
+// }
+//}
+
+UserBar.prototype.move = function (dx){
+  var x = (this.x + dx) % 760;
+}
+
+UserBar.prototype.shoot = function (){
+  var heart = new Heart;
+}
+
+
+function Heart (){
+  xHeart = userBar.x;
+  yHeart = userBar.y;
+}
+
+Heart.prototype.move = function (){
+  yHeart--;
+}
+
+// Heart.prototype.hit = function (){
+//   if ( xHeart === xTweet && yHeart === yTweet){
+
+//   } else if (xHeart === xRocket && yHeart === yRocket){
+
+//   } else continue;
 // }
 
-function Game (){
-  var trump = new Character (20, "Trump", "Kim");
-  var kim = new Character (780, "Kim", "Trump");
 
+
+function Game (){
+  setTimeout(function(){
+    var trump = new Character (20, "Trump", "Kim");
+    var kim = new Character (780, "Kim", "Trump");
+    var userBar = new UserBar;
+  }, 180000)
 }
 
 var game = new Game;
