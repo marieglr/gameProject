806c77a6e8313ec176c1e2b4985b980802fd8af4
Marie Gillier <magillier@hotmail.fr>
2018-10-12 00:12:31 +0200

    styled timer and added progress bar

--- main.js
+++ main.js
@@ -27,18 +27,13 @@ var board = new Board();
 
 //Timer
 var timer = 90;
-function timerCountdown() {
-  var interval = setInterval(function() {
-    timer--;
-    $(".timer span").text(timer);
-    if (timer <= 0){
-      clearInterval(interval);
-    }
-  }, 1000);
-}
-
-//run Timer
-timerCountdown();
+var timerInterval = setInterval(function() {
+  timer--;
+  $(".timer span").text(timer);
+  if (timer <= 0) {
+    clearInterval(timerInterval);
+  }
+}, 1000);
 
 //GENERAL RE-USABLE FUNCTIONS
 
@@ -69,7 +64,7 @@ function Character(x, img, imgWidth) {
   this.ego = 100;
   this.img = img;
   this.width = imgWidth * zoomFactor;
-  this.height = 100 * 1.5 * zoomFactor;
+  this.height = 150 * zoomFactor;
 }
 
 // Characters creation
@@ -77,13 +72,13 @@ function Character(x, img, imgWidth) {
 var trumpimg = new Image();
 trumpimg.src = "images/Trump.png";
 //creation of Trump
-var trump = new Character(25, trumpimg, 98 * 1.5);
+var trump = new Character(25, trumpimg, 147);
 
 //Load image for Kim
 var kimimg = new Image();
 kimimg.src = "images/KimCharacter.png";
 //creation of Kim
-var kim = new Character(canvas.width - 150, kimimg, 81 * 1.5);
+var kim = new Character(canvas.width - 150, kimimg, 122);
 
 //------------------------------------------------------------------------------------------------------------------------
 //CHARACTERS PROJECTILES
@@ -91,7 +86,7 @@ var kim = new Character(canvas.width - 150, kimimg, 81 * 1.5);
 
 //TWEETS object and methods
 function Tweet() {
-  this.x = trump.x + trump.width;
+  this.x = trump.x + trump.width + 5;
   this.y = trump.y + 25;
   this.img = new Image();
   this.img.src = "./images/Logo-Twitter.png";
@@ -112,11 +107,9 @@ function SlowTweet(x, img, width, height, isIntercepted) {
   this.y = trump.y + 45;
 }
 
-SlowTweet.prototype.move = function(){
+SlowTweet.prototype.move = function() {
   this.x += slowSpeed;
-}
-
-
+};
 
 //ROCKETS object and methods
 function Rocket() {
@@ -134,22 +127,21 @@ Rocket.prototype.move = function() {
 };
 
 //Create a second type of rockets that will move slower
-SlowRocket.prototype = Object.create(Rocket.prototype)
+SlowRocket.prototype = Object.create(Rocket.prototype);
 function SlowRocket(x, img, width, height, isIntercepted) {
   Rocket.call(this, x, img, width, height, isIntercepted);
   this.y = kim.y + 30;
 }
 
-SlowRocket.prototype.move = function(){
+SlowRocket.prototype.move = function() {
   this.x -= slowSpeed;
-}
-
+};
 
 //HAVE THE CHARACTERS RANDOMLY SHOOT PROJECTILES
 var tweets = [];
 var rockets = [];
 
-  //This function adds amo in the array that will serve as a reserve for shootings
+//This function adds amo in the array that will serve as a reserve for shootings
 function addTweets() {
   var newTweet = new Tweet();
   var newSlowTweet = new SlowTweet();
@@ -164,22 +156,22 @@ function addRockets() {
   rockets.push(newSlowRocket);
 }
 
-  //This anonymous function randomly calls the addTweets and addRockets functions (setInterval can only have regular interval, I want irregularity)
+//This anonymous function randomly calls the addTweets and addRockets functions (setInterval can only have regular interval, I want irregularity)
 (function loop() {
-    var rand = Math.round(Math.random() * (6000 - 2000)) + 2000;
-    setTimeout(function() {
-            addTweets();
-            loop();
-    }, rand);
-}());
+  var rand = Math.round(Math.random() * (6000 - 2000)) + 2000;
+  setTimeout(function() {
+    addTweets();
+    loop();
+  }, rand);
+})();
 
 (function loop() {
-    var rand = Math.round(Math.random() * (6000 - 2000)) + 2000;
-    setTimeout(function() {
-            addRockets();
-            loop();
-    }, rand);
-}());
+  var rand = Math.round(Math.random() * (6000 - 2000)) + 2000;
+  setTimeout(function() {
+    addRockets();
+    loop();
+  }, rand);
+})();
 
 //------------------------------------------------------------------------------------------------------------------------
 //USER BAR OBJECT AND METHODS
@@ -229,76 +221,65 @@ var game = setTimeout(function() {
 
 //function drawLoop (){
 var drawLoop = setInterval(function() {
+  //erase the old drawings
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    //erase the old drawings
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  //redraw the background, the characters and user bar
+  draw(board);
+  draw(userBar);
+  draw(trump);
+  draw(kim);
 
-    //redraw the background, the characters and user bar
-    draw(board);
-    draw(userBar);
-    draw(trump);
-    draw(kim);
+  //Manage hearts, rockets and tweets drawing and collisions
+  heartsLogic();
+  rocketsLogic();
+  tweetsLogic();
 
-    //Manage hearts, rockets and tweets drawing and collisions
-    heartsLogic();
-    rocketsLogic();
-    tweetsLogic();
+  //erase tweets that have been intercepted by a heart
+  tweets = tweets.filter(function(oneTweet) {
+    return !oneTweet.isIntercepted;
+  });
 
-    //Redraw the ego points of each character after being hit
-    ctx.font = "30px 'Kanit'";
-    ctx.fillStyle = "#940000";
-    ctx.fillText("Ego level: " + trump.ego, 40, 30);
-    ctx.fillText("Ego level: " + kim.ego, kim.x - 100, 30);
-    ctx.strokeStyle = "#931f20";
-    ctx.strokeText("Ego level: " + trump.ego, 40, 30);
-    ctx.strokeText("Ego level: " + kim.ego, kim.x - 100, 30);
+  rockets = rockets.filter(function(oneRocket) {
+    return !oneRocket.isIntercepted;
+  });
+}, 1000 / 60);
 
-    //erase tweets that have been intercepted by a heart
-    tweets = tweets.filter(function(oneTweet) {
-      return !oneTweet.isIntercepted;
+function heartsLogic() {
+  //Manage heart drawing and interaction with projectiles
+  hearts.forEach(function(oneHeart) {
+    //manage tweets interaction with hearts
+    tweets.forEach(function(oneTweet) {
+      if (collision(oneHeart, oneTweet)) {
+        oneTweet.isIntercepted = true;
+      }
     });
 
-    rockets = rockets.filter(function(oneRocket) {
-      return !oneRocket.isIntercepted;
+    //manage rockets interaction with hearts
+    rockets.forEach(function(oneRocket) {
+      if (collision(oneHeart, oneRocket)) {
+        oneRocket.isIntercepted = true;
+      }
     });
 
-  }, 1000 / 60);
+    //Draw hearts
+    draw(oneHeart);
+    oneHeart.move();
+  });
+}
 
-
-  function heartsLogic(){
-        //Manage heart drawing and interaction with projectiles
-        hearts.forEach(function(oneHeart) {
-          //manage tweets interaction with hearts
-          tweets.forEach(function(oneTweet) {
-            if (collision(oneHeart, oneTweet)) {
-              oneTweet.isIntercepted = true;
-            }
-          });
-    
-          //manage rockets interaction with hearts
-          rockets.forEach(function(oneRocket) {
-            if (collision(oneHeart, oneRocket)) {
-              oneRocket.isIntercepted = true;
-            }
-          });
-    
-          //Draw hearts
-          draw(oneHeart);
-          oneHeart.move();
-        });
-  }
-
-function rocketsLogic(){
+function rocketsLogic() {
   //manage rockets drawings and interaction with target
   rockets.forEach(function(oneRocket) {
     if (collision(oneRocket, trump)) {
       oneRocket.isIntercepted = true;
       trump.ego -= 5;
+      document.getElementById("trump-ego").setAttribute("value", trump.ego);
 
-      //If Trump loses face: GAME OVER
+      //If Trump loses face: GAME OVER - NUCLEAR WAR: YOUR KIDS WILL BE BORN WITH THREE LEGS AND ONLY ONE EYE
       if (trump.ego <= 0) {
         clearInterval(drawLoop);
-        timer = 1;
+        clearInterval(timerInterval);
       }
     }
 
@@ -307,17 +288,19 @@ function rocketsLogic(){
   });
 }
 
-function tweetsLogic(){
+function tweetsLogic() {
   //manage tweets drawings and interaction with target
   tweets.forEach(function(oneTweet) {
     //if a tweet hits Kim, kim's ego gets damage
     if (collision(oneTweet, kim)) {
       oneTweet.isIntercepted = true;
       kim.ego -= 5;
+      document.getElementById("kim-ego").setAttribute("value", kim.ego);
+
       //if Kim loses face: GAME OVER - NUCLEAR WAR: YOUR KIDS WILL BE BORN WITH THREE LEGS AND ONLY ONE EYE
       if (kim.ego <= 0) {
         clearInterval(drawLoop);
-        timer = 1;
+        clearInterval(timerInterval);
       }
     }
 
